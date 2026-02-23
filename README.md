5754b4f2fa33d584796d796832ecced07d356026
Michael Nixon <michael@edukits.co>
2026-02-23 15:25:15 -0800

    feat: set up project to build multiple sub-sketches

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # ME 210 Final Project
 
 A curling robot.
+
+Requires [PlatformIO](https://platformio.org/).
+
+## Uploading
+
+- Build main firmware: `pio run -e main`
+- Upload main firmware `pio  run -e main -t upload`
+
+Use the `-e <environment>` flag to select the desired environment.
+
+We'll use this to independently test/operate subsystems and integrations.
--- apps/main/main.cpp
+++ apps/main/main.cpp
@@ -6,10 +6,16 @@ int myFunction(int, int);
 void setup() {
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
+
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making
+  delay(1000);
 }
 
 // put function definitions here:
